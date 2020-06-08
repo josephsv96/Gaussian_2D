ca11a9ad15607bb54062149e9e128b73e5542ac7
Joseph Sebastian <josephsv@gmail.com>
2020-06-08 21:49:03 +0200

    SegmentationMap

--- gaussian_classifier.py
+++ gaussian_classifier.py
@@ -1,9 +1,39 @@
 import numpy as np
+from tensorflow.keras.utils import to_categorical
 
 
-class Gaussian_2D:
+class SegmentaionMap:
     def __init__(self, annot_arr):
         self.annot_arr = annot_arr
 
+    def arr_to_categorical(self):
+        annot_arr_cat = to_categorical(
+            self.annot_arr, num_classes=None, dtype='float32')
+
+        return annot_arr_cat
+
     def get_gaussian(self):
-        pass
+        annot_arr = self.arr_to_categorical()
+        return annot_arr
+
+    def activation_density(self, image_arr):
+        activation_densities = np.zeros(image_arr.shape[-1])
+        total_pixels = image_arr.shape[0] * image_arr.shape[1]
+
+        for i in range(image_arr.shape[-1]):
+            image = image_arr[:, :, i]
+            activated_pixels = image[image > 0]
+            activation_densities[i] = activated_pixels.shape[0] / total_pixels
+
+        return activation_densities
+
+    def get_density(self):
+        annot_arr = self.arr_to_categorical()
+        batch_size = annot_arr.shape[0]
+        densities_list = np.zeros([batch_size, annot_arr.shape[-1]])
+
+        for i in range(batch_size):
+            densities_list[i, :] = self.activation_density(
+                annot_arr[i, :, :, :])
+
+        return densities_list
