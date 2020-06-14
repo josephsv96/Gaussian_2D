0329f7f966700bb7add9dd43fac7cf4c7e3268f7
Joseph Sebastian <josephsv@gmail.com>
2020-06-14 12:48:49 +0200

    'batch_dist_working'

--- gaussian_classifier.py
+++ gaussian_classifier.py
@@ -7,6 +7,8 @@ from scipy.stats import norm
 class SegmentaionMap:
     def __init__(self, annot_arr):
         self.annot_arr = annot_arr
+        self.class_colors = ['black', '#259c14', '#4c87c6',
+                             '#737373', '#cbec24', '#f0441a', '#0d218f']
 
     def arr_to_categorical(self):
         annot_arr_cat = to_categorical(
@@ -59,6 +61,7 @@ class SegmentaionMap:
 
             j = j + 2
 
+    # PART 1
     def class_dist(self, low_threshold=0.15):
         densities_list = self.get_density()
         num_classes = densities_list.shape[-1]
@@ -68,9 +71,7 @@ class SegmentaionMap:
         # Average of prediction over all classes
         class_dist = np.zeros([num_classes, num_annots, num_classes])
         for i in range(num_annots):
-            # print(densities_list[i, :])
             class_index = np.where(densities_list[i, 1:] > low_threshold)[0][0]
-
             class_dist[class_index, i, 1:] = densities_list[i, 1:]
 
         class_dist = np.sum(class_dist, axis=1)
@@ -80,8 +81,7 @@ class SegmentaionMap:
     def show_class_dist(self):
         densities_list = self.class_dist()
         num_classes = densities_list.shape[0]
-        class_colors = ['black', '#259c14', '#4c87c6',
-                        '#737373', '#cbec24', '#f0441a', '#0d218f']
+        class_colors = self.class_colors
         plt.figure(figsize=(12, 24))
 
         for i in range(num_classes):
@@ -93,7 +93,8 @@ class SegmentaionMap:
             plt.ylabel('Prediciton')
             plt.ylim(0)
 
-    def batch_dist(self, low_threshold=0.15):
+    # PART 2
+    def batch_dist(self, low_threshold=0.15, low_cutoff=0.05):
         densities_list = self.get_density()
         batch_dist = densities_list.transpose()
         num_classes = densities_list.shape[1]
@@ -103,44 +104,59 @@ class SegmentaionMap:
             class_index = np.where(batch_dist[i, :] > low_threshold)[0]
             new_dist.append(densities_list[class_index, :])
 
+        # To remove very low values that disturb the distribution
+        for i in range(num_classes):
+            sample_x = new_dist[i]
+            for j in range(num_classes):
+                x_values = sample_x[:, j]
+                mean = np.mean(x_values)
+                if mean < low_cutoff:
+                    new_dist[i][:, j] = np.zeros(x_values.shape)
+        # This needs to be improved
         return new_dist, batch_dist
 
-    # def get_norm_values(self, x_values):
-    #     mean = np.mean(x_values)
-    #     std = np.std(x_values)
-    #     y_values = norm(mean, std).pdf(x_values)
+    def get_norm_values(self, x_values, plot_type='a'):
+        x_values_norm = 1
+        if plot_type != 'a':
+            x_values_norm = np.linalg.norm(x_values)
+        x_values = np.sort(x_values / x_values_norm)
+        mean = np.mean(x_values)
+        std = np.std(x_values)
+        y_values = norm.pdf(x_values, mean, std)
 
-    #     return y_values
+        return x_values, y_values, mean, std
 
-    def show_batch_dist(self):
+    def show_batch_dist(self, show_bg=False, max_y_limit=10, plot_type='a'):
         new_dist, _ = self.batch_dist()
         num_classes = len(new_dist)
-        class_colors = ['black', '#259c14', '#4c87c6',
-                        '#737373', '#cbec24', '#f0441a', '#0d218f']
+        if show_bg:
+            class_list = np.arange(num_classes)
+        else:
+            class_list = np.arange(1, num_classes)
 
-        # plt.figure(figsize=(10, 20))
-        for j in range(num_classes):
-            plt.figure(figsize=(10, 20))
-            plt.subplot(num_classes, 1, j+1)
-            # j = 4
+        class_colors = self.class_colors
 
-            sample_x = new_dist[j]
-            sample_y = np.zeros(new_dist[j].shape)
+        plt.figure(figsize=(10, 24))
+        for i in class_list:
+            plt.subplot(num_classes, 1, i+1)
+            sample_x = new_dist[i]
+            sample_y = np.zeros(sample_x.shape)
+            legends = []
+            for j in class_list:
+                x_values, y_values, mean, std = self.get_norm_values(
+                    sample_x[:, j], plot_type)
 
-            for i in range(num_classes):
-                plt.scatter(sample_x[:, i], sample_y[:, i],
-                            color=class_colors[i])
+                if mean != 0:
+                    legends.append('Class_' + str(j))
+                    if plot_type == 'a':
+                        plt.scatter(sample_x[:, j], sample_y[:, j],
+                                    color=class_colors[j])
 
-                x_values = np.sort(sample_x[:, i])
-                mean = np.mean(x_values)
-                std = np.std(x_values)
-
-                y_values = norm(mean, std).pdf(np.sort(sample_x[:, i]))
-
-                plt.plot(x_values, y_values, color=class_colors[i])
-
-            plt.legend(['Class_' + str(j)])
-            plt.xlabel('Classes')
-            plt.ylabel('Prediciton')
-            plt.ylim(0)
-            plt.show()
+                    plt.plot(x_values, y_values, color=class_colors[j])
+                    plt.axvline(mean, color=class_colors[j], linestyle='--')
+                    plt.legend(legends)
+            plt.xlabel('Frequency')
+            plt.ylabel('PDF')
+            plt.ylim(0, max_y_limit)
+            plt.xlim(0, 1)
+        plt.show()
